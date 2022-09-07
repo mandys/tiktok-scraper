545ba4cbc5f4261758a1c8de7e943f16de05519e
Maninder Singh <mandys@gmail.com>
2022-09-07 18:02:59 +0530

    updated for comment gathering

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const TikTokScraper = require('./build/index.js');
 
 (async () => {
     try {
-        const videoMeta = await TikTokScraper.getVideoMeta('https://www.tiktok.com/@karthiksunny100/video/7137578195238309126', {});
-        console.log(videoMeta);
+        const videoMeta = await TikTokScraper.getVideoMeta('https://www.tiktok.com/@karthiksunny100/video/7137578195238309126', {
+            //useTestEndpoints: true,
+			sessionList: ['sid_tt=f8950798d8a47d3ed03fbfaee4b8d267;']
+		});
+        videoMeta.collector[0].comments.forEach(function(comment) {
+            console.log(comment)
+        })
     } catch (error) {
         console.log(error);
     }
